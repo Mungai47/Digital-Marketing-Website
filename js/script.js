8185958a5c398e7550b2f34c782cb8762b7ce0cc
Joseph <Josephmburumungai99@gmail.com>
2023-05-20 21:44:50 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var button = document.getElementById("myButton");
+
+
+button.addEventListener("mouseover", function() {
+  button.innerText = "Let's Go!";
+});
+
+button.addEventListener("mouseout", function() {
+  button.innerText = "Start a Project?";
+});
+
+
+
+
